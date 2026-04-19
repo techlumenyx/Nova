8c7ae9b62af4a11796e58d25a64c485086a0023a
prnavvv <kadampranav2003@gmail.com>
2026-04-19 20:35:14 +0530

    mongo state logger

--- services/content/src/resolvers/queries/searchLabTests.ts
+++ services/content/src/resolvers/queries/searchLabTests.ts
@@ -1,11 +1,13 @@
 import { labTestService } from '../../services';
 import { logger } from '@nova/shared';
+import mongoose from 'mongoose';
 
 export const searchLabTests = async (
   _: unknown,
   { query, limit }: { query: string; limit?: number },
 ) => {
-  logger.info('[searchLabTests] resolver called', { query, limit });
+  const states = ['disconnected', 'connected', 'connecting', 'disconnecting'];
+  logger.info('[searchLabTests] resolver called', { query, limit, mongoState: states[mongoose.connection.readyState] });
   const result = await labTestService.search(query, limit);
   logger.info('[searchLabTests] result', { testCount: result.tests.length, packageCount: result.packages.length });
   return result;
